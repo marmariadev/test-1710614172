6451173fc56035d42eba47da46f3ebb7b088ccec
marmariadev <marmariadev@gmail.com>
2024-03-16 23:18:50 -0300

    Agregar endpoint GET para getAllUsers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import * as userController from '../controllers/userController';
 
 const router = Router();
 
+router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 
 export default router;
