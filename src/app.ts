9288dafc011818dac5cf09d1b1eaef2a548743b2
marmariadev <marmariadev@gmail.com>
2024-03-16 23:08:47 -0300

    Habilitar middleware express.json()

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
